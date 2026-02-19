385c7214bf4ac21010464b78d0ad144fa37d71fe
alevtina <alevtina@saturntech.gitlab.yandexcloud.net>
2026-02-19 08:43:05 +0300

    submodule with nginx

--- packages/twenty-widgets/src/app.tsx
+++ packages/twenty-widgets/src/app.tsx
@@ -32,7 +32,7 @@ const widgets: WidgetDefinition[] = [
   {
     id: 'example-widget-2',
     name: 'Example Widget',
-    description: 'Пример 2 микро для Twenty',
+    description: 'Пример 2 для Twenty',
     url: 'https://example.com'
   }
 ];
